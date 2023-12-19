d945f26ede819a4b21ee4695a6309fe6e2582418
yohan59235 <yohan.collinse@gmail.com>
2023-12-19 11:41:09 +0100

    add mariah carey

--- DeezTube/src/components/Top.jsx
+++ DeezTube/src/components/Top.jsx
@@ -2,8 +2,10 @@ import styles from "../styles/Top.module.css"
 
 function Top() {
   return (
-    <div>
-        
+    <div className={styles.christmas}>
+      <iframe className={styles.merrychristmas}  src="https://www.youtube.com/embed/aAkMkVFwAoo?si=UA5VZZsNlwb2YJ7D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <img className={styles.Topimg} src="src/assets/mariahCarey.jpg" alt="" />
+      
     </div>
   )
 }
